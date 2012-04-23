e40e8a0ba76e246f6adbb561fdd310c137d86d8d
Emil Stenqvist <emsten@gmail.com>
2012-04-23 17:24:50 +0200

    ...

--- setup.sh
+++ setup.sh
@@ -1,23 +1,36 @@
 #!/bin/bash
 
+# TODO: install carlhuda automatically
+# https://github.com/carlhuda/janus.git
+
+HOMEDIR=~
 BASEDIR=`dirname $0`
-FILES=".vim .vimrc.local .gvimrc.local .vimrc .inputrc .bash .gitconfig"
+
+FILES=".vimrc.local .gvimrc.local .vimrc .inputrc .bash .gitconfig"
+
+function absolute_path() {
+  local PARENT_DIR=$(dirname "$1")
+  cd "$PARENT_DIR"
+  local ABS_PATH="$(pwd)"/"$(basename $1)"
+  cd - > /dev/null
+
+  echo $ABS_PATH
+}
 
 for F in $FILES; do
-  if [ -e ~/$F ]; then
-    mv ~/$F{,.old}
+  if [ -e $HOMEDIR/$F ]; then
+    mv $HOMEDIR/$F{,.old}
     echo "moved existing $F to $F.old"
   fi
 
-  ln -s $BASEDIR/dotfiles/dot.$F ~/$F
+  ln -s $(absolute_path $BASEDIR/dotfiles/dot$F) $HOMEDIR/$F
 done
 
-if [ -e ~/bin ]; then
-  mv ~/$f{,.old}
+if [ -e $HOMEDIR/bin ]; then
+  mv $HOMEDIR/bin{,.old}
 fi
 
-ln -s $BASEDIR/bin ~/bin
+ln -s $BASEDIR/bin $HOMEDIR/bin
 
-cat $BASEDIR/dotfiles/dot.bash_login >> ~/.bash_login
-cat $BASEDIR/dotfiles/dot.bash_login >> ~/.bashrc
-cat $BASEDIR/dotfiles/dot.bash_login >> ~/.profile
+#   
+cat $BASEDIR/dotfiles/dot.bash_login >> $HOMEDIR/.bash_profile
